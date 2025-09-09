740535931831562b1ee721113cd0820f41bcfe3c
satyam pathak <satyampathak67@gmail.com>
2025-09-09 18:30:37 +0530

    progress to be made

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-6">
+      <h1 className="text-3xl font-bold text-blue-600 mb-6">
+        progress to be made
+      </h1>
+    </div>
+  )
+}
