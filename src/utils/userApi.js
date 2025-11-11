d3f9004499acc712549e5b301c3835dc703dac6a
Satyam Pathak <satyampathak67@gmail.com>
2025-11-11 19:24:49 +0530

    changed the url path in userapi

--- src/utils/userApi.js
+++ src/utils/userApi.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL+"/users" || "http://localhost:8000/users";
 
 export async function loginUser({ username, password }) {
   const res = await fetch(`${API_URL}/login`, {
